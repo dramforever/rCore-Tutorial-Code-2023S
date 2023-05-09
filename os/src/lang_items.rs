71bc640503b89c88b7e1fff0e1848b8a21a1f06c
dramforever <dramforever@live.com>
2023-05-09 18:35:47 +0800

    Add symbols for stack traces
    Based on new symbol table from rvind

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -9,6 +9,10 @@ global_asm!(
     "
         .weak rvind_data_start
         .weak rvind_data_end
+        .weak rvind_sym_start
+        .weak rvind_sym_end
+        .weak rvind_str_start
+        .weak rvind_str_end
 
         .section .rodata
         .p2align 3
@@ -16,13 +20,67 @@ global_asm!(
         .quad rvind_data_start
         .quad rvind_data_end - rvind_data_start
 
+    rvind_sym_start_addr:
+        .quad rvind_sym_start
+    rvind_sym_end_addr:
+        .quad rvind_sym_end
+    rvind_str_start_addr:
+        .quad rvind_str_start
+    rvind_str_end_addr:
+        .quad rvind_str_end
+
         .previous
     "
 );
 
+#[derive(Debug)]
+#[repr(C)]
+struct SymbolEntry {
+    code_offset: u32,
+    str_offset: u32,
+}
+
 extern "C" {
     static rvind_header: rvind_unwinder::Header;
     static stext: u8;
+    static rvind_sym_start_addr: *const SymbolEntry;
+    static rvind_sym_end_addr: *const SymbolEntry;
+    static rvind_str_start_addr: *const u8;
+    static rvind_str_end_addr: *const u8;
+}
+
+fn symbolize(pc: usize) -> Option<(&'static [u8], u32)> {
+    if unsafe { rvind_sym_start_addr }.is_null() {
+        return None;
+    }
+
+    let text_start = unsafe { &stext } as *const u8 as usize;
+
+    let offset: u32 = pc.checked_sub(text_start)?.try_into().ok()?;
+
+    let symtab: &'static [SymbolEntry] = unsafe {
+        core::slice::from_raw_parts(
+            rvind_sym_start_addr,
+            rvind_sym_end_addr.offset_from(rvind_sym_start_addr) as usize,
+        )
+    };
+
+    let strtab: &'static [u8] = unsafe {
+        core::slice::from_raw_parts(
+            rvind_str_start_addr,
+            rvind_str_end_addr.offset_from(rvind_str_start_addr) as usize,
+        )
+    };
+
+    let index = symtab
+        .partition_point(|sym| sym.code_offset <= offset)
+        .checked_sub(1)?;
+    let symbol = symtab.get(index)?;
+
+    let str = &strtab[symbol.str_offset as usize..];
+    let str_end = str.iter().position(|&x| x == 0)?;
+
+    Some((&str[..str_end], offset - symbol.code_offset))
 }
 
 extern "C" fn do_unwind(pc: usize, ra: usize, sp: usize, fp: usize) {
@@ -37,7 +95,16 @@ extern "C" fn do_unwind(pc: usize, ra: usize, sp: usize, fp: usize) {
                 frame: rvind_unwinder::CallFrame { pc, sp, fp },
             },
             &mut |frame| {
-                println!(" {:#x}", frame.pc);
+                if let Some((symbol, inner_offset)) = symbolize(frame.pc) {
+                    println!(
+                        " ({:#010x}) {} + {:#x}",
+                        frame.pc,
+                        symbol.escape_ascii(),
+                        inner_offset,
+                    );
+                } else {
+                    println!(" ({:#010x}) ???", frame.pc);
+                }
             },
         );
     }
--- os/src/rvind_bin.s
+++ os/src/rvind_bin.s
@@ -1,8 +1,21 @@
     .global rvind_data_start
     .global rvind_data_end
+    .global rvind_sym_start
+    .global rvind_sym_end
+    .global rvind_str_start
+    .global rvind_str_end
 
     .section .rodata
     .p2align 2
 rvind_data_start:
     .incbin "rvind.bin"
 rvind_data_end:
+
+    .p2align 2
+rvind_sym_start:
+    .incbin "rvind.bin.sym"
+rvind_sym_end:
+
+rvind_str_start:
+    .incbin "rvind.bin.str"
+rvind_str_end:
